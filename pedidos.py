f74ce67eeb0c90fa22e401fffd06875f3e82ddb7
EdySC97 <131077608+EdySC97@users.noreply.github.com>
2025-07-09 22:47:00 -0600

    Update pedidos.py

--- pedidos.py
+++ pedidos.py
@@ -154,9 +154,10 @@ def generar_ticket_pdf(mesa, personas, orden_id, items, total):
     pdf.set_font("Courier", size=7)
 
     # Encabezado
-    pdf.cell(0, 5, "====== BAR KAVIA ======", ln=True, align="C")
-    pdf.cell(0, 5, f"Mesa: {mesa}   Pers: {personas}", ln=True)
-    pdf.cell(0, 5, f"Orden: {str(orden_id)[:8]}", ln=True)
+    
+    pdf.cell(0, 5, "====== BAR KAVIA ======", style="B", ln=True, align="C")
+    pdf.cell(0, 5, f"Mesa: {mesa}   Pers: {personas}",style="B", ln=True)
+    pdf.cell(0, 5, f"Orden: {str(orden_id)[:8]}", style="B",ln=True)
     pdf.cell(0, 5, f"Fecha: {datetime.now():%Y-%m-%d %H:%M}", ln=True)
     pdf.cell(0, 5, "-" * 38, ln=True)
 
